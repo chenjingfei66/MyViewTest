09b4095d1b6538d13bf654b48f2d38cc3dd7a815
chenjingfei66 <1036710772@QQ.com>
2018-03-13 16:59:58 +0800

    First commit

---
+++ app/src/main/java/com/example/feifei/myviewtest/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.feifei.myviewtest;
+
+
+import android.graphics.Point;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //onMeature
+    private TextView text;
+    private Button b1;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+        MyFragment myFragment = new MyFragment();
+        MyFragment2 myFragment2 = new MyFragment2();
+        List<Fragment> list = new ArrayList<>();
+        list.add(myFragment);
+        list.add(myFragment2);
+        MyFragmentAdapter myFragmentAdapter = new MyFragmentAdapter(getSupportFragmentManager(),list);
+        viewPager.setAdapter(myFragmentAdapter);
+        viewPager.setCurrentItem(0);
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+        System.out.println(point.x);
+        System.out.println(point.y);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            int position;
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                    System.out.println("position---" + position);
+                    System.out.println("positionOffset---" + positionOffset);
+                    System.out.println("positionOffsetPixels---" + positionOffsetPixels);
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                this.position = position;
+                System.out.println("position---" + position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                System.out.println("state---" + state);
+            }
+        });
+    }
+
+    public void test(){
+        System.out.println("fffff");
+    }
+
+    private void initView() {
+        b1 = (Button) findViewById(R.id.b1);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+    }
+}
